0c2244fd039980bdab8f16c5fe7c33bcbd94dae3
Alex Dorchak <halfdome@fluke.com>
2016-10-20 14:55:11 -0700

    Driver support for GPIB device support

---
+++ drivers/gpib/include/config.h
@@ -0,0 +1,31 @@
+/* Define to enable debug spam to console in drivers */
+/* #undef GPIB_CONFIG_KERNEL_DEBUG */
+/* #define GPIB_CONFIG_KERNEL_DEBUG 1 */
+
+/* Define to enable pcmcia support in drivers */
+/* #undef GPIB_CONFIG_PCMCIA */
+
+/* Major number of device files */
+#define IBMAJOR 160
+
+/* Name of package */
+#define PACKAGE "linux-gpib"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "linux-gpib"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "linux-gpib 3.2.21-fluke"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "linux-gpib"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.2.21-fluke"
+
+/* Version number of package */
+#define VERSION "3.2.21-fluke"
+
