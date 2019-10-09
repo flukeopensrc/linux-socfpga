7eb8b0a429da3e5f6dcacd004d8abb96175efefe
Frank Mori Hess <fmh6jj@gmail.com>
2019-10-08 20:54:40 -0400

    Declare fluke logos of various sizes.

--- include/linux/linux_logo.h
+++ include/linux/linux_logo.h
@@ -47,7 +47,9 @@ extern const struct linux_logo logo_superh_vga16;
 extern const struct linux_logo logo_superh_clut224;
 extern const struct linux_logo logo_m32r_clut224;
 extern const struct linux_logo logo_spe_clut224;
-extern const struct linux_logo logo_fluke_clut224;
+extern const struct linux_logo logo_fluke_clut224_480x272;
+extern const struct linux_logo logo_fluke_clut224_640x480;
+extern const struct linux_logo logo_fluke_clut224_800x480;
 
 extern const struct linux_logo *fb_find_logo(int depth);
 #ifdef CONFIG_FB_LOGO_EXTRA
